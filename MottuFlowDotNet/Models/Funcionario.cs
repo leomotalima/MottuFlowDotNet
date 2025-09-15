519e60ba856a1ddb2d48cb93e944e0d3f985c728
leomotalima <leomotalima@github.com>
2025-09-15 19:43:30 -0300

    Atualiza Funcionario e RegistroStatus, remove arquivos de Sonar

--- MottuFlowDotNet/Models/Funcionario.cs
+++ MottuFlowDotNet/Models/Funcionario.cs
@@ -7,9 +7,9 @@ namespace MottuFlowApi.Models
         public string Cpf { get; set; } = null!;
         public string Cargo { get; set; } = null!;
         public string Telefone { get; set; } = null!;
-        public string Email { get; set; } = null!;           // corresponde ao campo email
+        public string Email { get; set; } = null!;           
         public string Senha { get; set; } = null!;
-        public string? RefreshToken { get; set; }           // corresponde ao refresh_token
-        public DateTime? ExpiracaoRefreshToken { get; set; } // corresponde ao expiracao_refresh_token
+        public string? RefreshToken { get; set; }           
+        public DateTime? ExpiracaoRefreshToken { get; set; } 
     }
 }
